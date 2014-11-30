f34464145bcb1d97c063cf903e15fc88061aea85
Alaa Qutaish <alaa.qutaish@gmail.com>
2014-11-30 01:35:09 +0100

    add loader module

---
+++ lib/echos/loader.rb
@@ -0,0 +1,26 @@
+require 'json'
+require 'multi_json'
+
+module Echos
+  class Loader
+
+    def self.load_file(file)
+      if valid_file?(file)
+        content = IO.read(file)
+
+        begin
+          MultiJson.load(content, symbolize_keys: true)
+        rescue MultiJson::ParseError => exception
+          Echos::logger.info(exception.cause)
+        end
+      end
+    end
+
+    private
+
+    def self.valid_file?(file)
+      File.file?(file) && File.readable?(file)
+    end
+  end
+end
+
