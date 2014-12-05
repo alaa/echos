0b8d273bce3b9224aca6781b171b16bcfa6ed8bb
Alaa Qutaish <alaa.qutaish@wimdu.com>
2014-12-05 17:52:01 +0100

    refactor tests

--- spec/check_spec.rb
+++ spec/check_spec.rb
@@ -1,22 +1,21 @@
-require_relative "../lib/check"
+module Echos
+  describe Check do
+    let(:check_disk) { Check.new(name: :check_disk, command: "df -h") }
+    let(:check_mem) { Check.new(name: :check_memory, command: "free -m") }
 
-describe Check do
-  let(:check_disk) { Check.new(name: :check_disk, command: "df -h") }
-  let(:check_mem) { Check.new(name: :check_memory, command: "free -m") }
+    it "adds checks" do
+      expect(check_disk.name).to eql :check_disk
+      expect(check_disk.command).to eql "df -h"
+    end
 
-  it "adds checks" do
-    expect(check_disk.name).to eql :check_disk
-    expect(check_disk.command).to eql "df -h"
-  end
+    it "understand equality" do
+      expect(check_disk == check_disk).to be_true
+      expect(check_disk == check_mem).to be_false
+      expect(check_disk == Object.new).to be_false
+    end
 
-  it "understand equality" do
-    expect(check_disk == check_disk).to be_true
-    expect(check_disk == check_mem).to be_false
-    expect(check_disk == Object.new).to be_false
-  end
-
-  it "understand uniqueness" do
-    expect([check_disk, check_disk, check_mem].uniq.size).to eq 2
+    it "understand uniqueness" do
+      expect([check_disk, check_disk, check_mem].uniq.size).to eq 2
+    end
   end
 end
-
--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -1,49 +1,50 @@
-require_relative "../lib/template"
+module Echos
 
-describe Template do
-  let(:check_disk) { Check.new(name: :check_disk, command: "df -h") }
-  let(:check_mem) { Check.new(name: :check_memory, command: "free -m") }
+  describe Template do
+    let(:check_disk) { Check.new(name: :check_disk, command: "df -h") }
+    let(:check_mem) { Check.new(name: :check_memory, command: "free -m") }
 
-  it "initializes with a name" do
-    template = Template.new(:basic)
-    expect(template.name).to eq :basic
-  end
-
-  context "Add checks" do
-    it "adds check objects" do
+    it "initializes with a name" do
       template = Template.new(:basic)
-
-      template.add_checks([check_disk, check_disk, check_mem])
-      expect(template.checks.count).to eq 2
+      expect(template.name).to eq :basic
     end
 
-    it "raises TypeError if checks array is not a Check Object" do
-      template = Template.new(:basic)
-      expect(template.add_checks([Object.new])).to be_false
-    end
-  end
+    context "Add checks" do
+      it "adds check objects" do
+        template = Template.new(:basic)
 
-  context "Add templates to each other" do
-    it "can sum templates together" do
-      template1 = Template.new(:basic1)
-      template1.add_checks([check_disk])
+        template.add_checks([check_disk, check_disk, check_mem])
+        expect(template.checks.count).to eq 2
+      end
 
-      template2 = Template.new(:basic2)
-      template2.add_checks([check_mem])
-
-      template_1_2 = template1 + template2
-      expect(template_1_2.name).to eq "basic1_and_basic2"
-      expect(template_1_2.checks).to eq [check_disk, check_mem]
+      it "raises TypeError if checks array is not a Check Object" do
+        template = Template.new(:basic)
+        expect(template.add_checks([Object.new])).to be_false
+      end
     end
 
-    it "does not sum itself" do
-      template1 = Template.new(:basic1)
-      template1.add_checks([check_disk])
-      template_1_1 = template1 + template1
+    context "Add templates to each other" do
+      it "can sum templates together" do
+        template1 = Template.new(:basic1)
+        template1.add_checks([check_disk])
 
-      expect(template_1_1.name).to eq :basic1
-      expect(template_1_1.checks).to eq [check_disk]
-      expect{ template1 + Object.new }.to raise_error
+        template2 = Template.new(:basic2)
+        template2.add_checks([check_mem])
+
+        template_1_2 = template1 + template2
+        expect(template_1_2.name).to eq "basic1_and_basic2"
+        expect(template_1_2.checks).to eq [check_disk, check_mem]
+      end
+
+      it "does not sum itself" do
+        template1 = Template.new(:basic1)
+        template1.add_checks([check_disk])
+        template_1_1 = template1 + template1
+
+        expect(template_1_1.name).to eq :basic1
+        expect(template_1_1.checks).to eq [check_disk]
+        expect{ template1 + Object.new }.to raise_error
+      end
     end
   end
 end
