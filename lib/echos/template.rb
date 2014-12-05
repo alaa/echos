971716bcf2e3a7871657c791b5b84e71d32b4b58
Alaa Qutaish <alaa.qutaish@wimdu.com>
2014-12-05 17:51:52 +0100

    wrap template into module

--- lib/echos/template.rb
+++ lib/echos/template.rb
@@ -1,32 +1,33 @@
-class Template
-  attr_reader :name, :checks
+module Echos
+  class Template
+    attr_reader :name, :checks
 
-  def initialize(name)
-    @name = name
-    @checks ||= []
+    def initialize(name)
+      @name = name
+      @checks ||= []
+    end
+
+    def add_checks(checks)
+      return false if checks.any? { |c| c.class != Check }
+      checks_dup = checks.dup.uniq
+      @checks.concat(checks_dup)
+    end
+
+    def +(other)
+      raise TypeError if other.class != self.class
+      return self if self == other
+
+      self.name = [self.name, other.name].map!(&:to_s).join('_and_')
+      self.checks = (self.checks + other.checks).uniq
+      self
+    end
+
+    def ==(other)
+      return false unless self.class === other
+      self.name == other.name && self.checks == other.checks
+    end
+
+    private
+    attr_writer :name, :checks
   end
-
-  def add_checks(checks)
-    return false if checks.any? { |c| c.class != Check }
-    checks_dup = checks.dup.uniq
-    @checks.concat(checks_dup)
-  end
-
-  def +(other)
-    raise TypeError if other.class != self.class
-    return self if self == other
-
-    self.name = [self.name, other.name].map!(&:to_s).join('_and_')
-    self.checks = (self.checks << other.checks).flatten.uniq
-    self
-  end
-
-  def ==(other)
-    return false unless self.class === other
-    self.name == other.name && self.checks == other.checks
-  end
-
-  private
-  attr_writer :name, :checks
 end
-
