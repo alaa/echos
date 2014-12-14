37c1138ff2951fc76d5b8d498e4a63a9e42700fe
Alaa Qutaish <alaa.qutaish@gmail.com>
2014-12-14 22:42:23 +0100

    refactor command

--- lib/echos/command.rb
+++ lib/echos/command.rb
@@ -4,39 +4,35 @@ require 'json'
 module Echos
   class Command
 
-    attr_reader :name, :command, :timeout, :child, :handlers
-    # make readers private
-
-    def initialize(name, command, handlers, timeout)
-      @name, @command, @hanlders, @timeout = name, command, handlers, timeout
+    def initialize(command:, timeout:)
+      @command, @timeout = command, timeout
     end
 
     def execute!
       begin
-        puts command
-        @child = POSIX::Spawn::Child.new(command, timeout.to_s)
-        success?
+        @process = POSIX::Spawn::Child.new(command, timeout: timeout)
+        completed_command
 
       rescue POSIX::Spawn::TimeoutExceeded => e
-        Echos::logger.error "Command #{command} timedout!"
         Echos::logger.error e
+        not_completed_command
       end
-    end
-
-    def packet
-      packet = Echos::Packet.new(name, handlers, child)
-      success? ? packet.success : packet.timeout
     end
 
     private
+    attr_accessor :command, :timeout, :process
 
-    def success?
-      begin
-        child.status.success?
-      rescue
-        false
-      end
+    def completed_command
+      { process_stdout: process.out,
+        process_stderr: process.err,
+        process_exitstatus: process.status.exitstatus,
+        process_runtime: process.runtime,
+        process_pid: process.status.pid }
     end
+
+    def not_completed_command
+      { process_runtime: -1 }
+    end
+
   end
 end
-
