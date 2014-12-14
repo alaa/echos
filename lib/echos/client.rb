db2b5ab36514baf7c6fa181449064054fd092a37
Alaa Qutaish <alaa.qutaish@gmail.com>
2014-12-14 22:42:17 +0100

    refactor client

--- lib/echos/client.rb
+++ lib/echos/client.rb
@@ -3,46 +3,38 @@ require 'json'
 
 module Echos
   class Client
-    # these should be private.
-    attr_reader :checks, :queue
 
     def initialize(checks, queue)
-      @checks = checks
-      @queue = queue
+      @checks, @queue = checks, queue
     end
 
     def start!
       EM.run do
         checks.each do |check|
-          # inside a module there is no need to be explict about it
+          check_obj = Check.new(check.last.merge(name: check.first))
+          puts check_obj.interval
 
-          check_object = Echos::Check.new(check.last.merge(name: check.first))
-
-          # interval is a data of check that is accessed from outside, it should belong to check object.
-          # the interval actually belongs to the eventMachine.
-
-          EM.add_periodic_timer(check_object.interval) do
-            EM.defer(operation(check_object), callback)
+          EM.add_periodic_timer(check_obj.interval) do
+            EM.defer(operation(check_obj), callback(check_obj))
           end
+
         end
       end
     end
 
     private
+    attr_reader :checks, :queue
 
     def operation(check)
-      Proc.new do
-        command = Echos::Command.new(check.name, check.command, check.handlers, check.timeout)
-        command.execute!
-        command.packet
-      end
+      Proc.new { check.execute! }
     end
 
-    def callback
+    def callback(check)
       Proc.new do |packet|
         queue.publish(packet.to_json)
       end
     end
+
   end
 end
 
