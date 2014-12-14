f4eded09f2bb9ca0e441f1494cd7f23f6be67270
Alaa Qutaish <alaa.qutaish@gmail.com>
2014-12-14 22:42:10 +0100

    refactor check

--- lib/echos/check.rb
+++ lib/echos/check.rb
@@ -1,22 +1,18 @@
 module Echos
-  # Understands how to read a config file. and to give check
-  # The class is the config the data is an Array of Strucuts
-
   class Check
-    # consider converting this into a Strucut
-
-    attr_reader :name, :timeout, :handlers, :path, :interval
 
     DEFAULT_CHECK_TIMEOUT = 1
-    DEFFAULT_INTERVAL_VALUE = 10
+    DEFFAULT_INTERVAL_VALUE = 5
     DEFAULT_HANDLER = "default_handler"
 
-    def initialize( name:,
-                    command:,
-                    timeout: DEFAULT_CHECK_TIMEOUT,
-                    handlers: DEFAULT_HANDLER,
-                    path: false,
-                    interval: DEFFAULT_INTERVAL_VALUE )
+    attr_reader :interval
+
+    def initialize(name:,
+                   command:,
+                   timeout: DEFAULT_CHECK_TIMEOUT,
+                   handlers: DEFAULT_HANDLER,
+                   path: false,
+                   interval: DEFFAULT_INTERVAL_VALUE )
       @name = name
       @command = command
       @timeout = timeout
@@ -25,19 +21,36 @@ module Echos
       @interval = interval
     end
 
+    def execute!
+      execute_object = Command.new(command: command_with_path, timeout: timeout)
+      process_info = execute_object.execute!
+      merge_check_info(process_info)
+    end
+
+    private
+    attr_reader :name, :command, :timeout, :handlers, :path
+
     def ==(other)
       return false unless self.class === other
-      self.name == other.name && self.command == other.command
+      self.name == other.name && self.command_with_path == other.command_with_path
     end
 
     def hash
       [self.name, self.command].hash
     end
 
-    def command
-      path ? (path + @command) : @command
+    def command_with_path
+      path ? (path + command) : command
     end
 
+    def merge_check_info(hash)
+      hash.merge(check_name: name,
+                 command: command_with_path,
+                 timeout: timeout,
+                 handlers: handlers,
+                 interval: interval,
+                 timestamp: Time.now)
+    end
   end
 end
 
