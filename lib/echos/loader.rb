cbad71d02560bf5a0188101e3f9230b498e00caf
Alaa Qutaish <alaa.qutaish@gmail.com>
2014-12-18 00:04:35 +0100

    refactor loader

--- lib/echos/loader.rb
+++ lib/echos/loader.rb
@@ -5,16 +5,13 @@ module Echos
   class Loader
 
     def self.load_file(file)
-      if valid_file?(file)
-        content = IO.read(file)
+      raise Exception unless valid_file?(file)
 
-        begin
-          MultiJson.load(content, symbolize_keys: true)
+      content = IO.read(file)
+      MultiJson.load(content, symbolize_keys: true)
 
-        rescue MultiJson::ParseError => exception
-          Echos::logger.info(exception.cause)
-        end
-      end
+    rescue MultiJson::ParseError => exception
+      Echos::logger.info(exception.cause)
     end
 
     private
