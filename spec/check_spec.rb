0dfe818afb86947f50f3dbccc30f766e8477a187
Alaa Qutaish <alaa.qutaish@gmail.com>
2014-12-28 00:14:31 +0100

    refactor check spec

--- spec/check_spec.rb
+++ spec/check_spec.rb
@@ -3,14 +3,27 @@ require 'spec_helper'
 module Echos
   describe Check do
 
-    let(:check_disk1) { Check.new(name: :check_disk1, command: "df -h") }
-    let(:check_disk2) { Check.new(name: :check_disk2, command: "df -h") }
-    let(:check_mem) { Check.new(name: :check_memory, command: "free -m") }
+    let(:check_disk1) { Check.new(name: 'check_disk1',
+                                  options: {'command' => 'df -h'} ) }
 
-    it "understands equality" do
-      expect(check_disk1 == check_disk2).to be_true
-      expect(check_disk1 == check_mem).to be_false
-      expect(check_disk1 == Object.new).to be_false
+    let(:check_disk2) { Check.new(name: 'check_disk2',
+                                  options: {'command' => 'df -h'} ) }
+
+    let(:check_mem) { Check.new(name: 'check_memory',
+                                options: {'command' => 'free -m'} ) }
+
+    describe "#==" do
+      it "should be equal to similar check" do
+        expect(check_disk1).to eq check_disk2
+      end
+
+      it "should not be equal with different command" do
+        expect(check_disk1).to_not eq check_mem
+      end
+
+      it "returns false with equality with different object type" do
+        expect(check_disk1).to_not eq Object.new
+      end
     end
 
     it "understands uniqueness" do
