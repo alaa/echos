cf45b8f8c327ba2497b1011f6bf55ecaef4224c2
Alaa Qutaish <alaa.qutaish@gmail.com>
2015-03-19 01:01:40 +0100

    add scheduler spec

---
+++ spec/scheduler_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+module Echos
+  describe Scheduler do
+    let(:check1) {
+      [ name: 'check1',
+        options: { command: 'dummyCommand' } ]
+    }
+
+    let(:check2) {
+      [ name: 'check2',
+        options: { command: 'dummyCommand2' } ]
+    }
+
+    let(:bus) { Class.new }
+
+    subject { Scheduler.new(check1 + check2, bus) }
+
+    it 'responds to execute' do
+      expect(subject).to respond_to(:start!)
+    end
+
+    it "calls EventMachine" do
+      expect(EM).to receive(:run)
+      subject.start!
+    end
+  end
+end
